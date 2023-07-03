6847c54243a8d9cc145ca4a47ac2b0726db648e2
Robbie Irvine <rni2000@hw.ac.uk>
2023-07-03 23:27:54 +0100

    Created login data template

---
+++ login-data-template.js
@@ -0,0 +1,18 @@
+// Copy and rename this file to "login-data.js" to have it recognised in the script!
+// Be careful not to add sensitive data to the template if you are pushing changes
+
+// The Lemmy instance you are taking your user data from
+let source_instance = {
+    username_or_email: "",
+    password: "",
+    url: "https://"
+}
+
+// The Lemmy instance you are copying your user data to
+let destination_instance = {
+    username_or_email: "",
+    password: "",
+    url: "https://"
+}
+
+export { source_instance, destination_instance };
