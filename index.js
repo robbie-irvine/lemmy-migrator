014ee3768b33212c724b795098e1c35156861fe4
Robbie Irvine <rni2000@hw.ac.uk>
2023-07-03 23:18:37 +0100

    Added login functionality

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+import { LemmyHttp } from 'lemmy-js-client';
+import { source_instance, destination_instance } from '../login-data.mjs';
 
-//TODO: make the program
-console.log("Hello world!");
+let baseUrl = source_instance.url;
+let client = new LemmyHttp(baseUrl);
+let loginForm = {
+  username_or_email: source_instance.username_or_email,
+  password: source_instance.password,
+};
+
+async function connect() {
+    let login = await client.login(loginForm).catch((e) => { console.error(e); return false; });
+    
+    console.log(login.jwt)
+}
+
+connect()
