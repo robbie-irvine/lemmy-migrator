8e7c5f1c5f3c2663cdafc0e5558c022d379a3e83
Robbie Irvine <rni2000@hw.ac.uk>
2023-07-04 07:03:02 +0100

    Added support for 2FA through console prompt

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 import { LemmyHttp } from 'lemmy-js-client';
 import { source_instance, destination_instance } from './login-data.js';
+import ps from 'prompt-sync';
+
+let prompt = ps();
 
 let baseUrl = source_instance.url;
 let client = new LemmyHttp(baseUrl);
-let loginForm = {
-  username_or_email: source_instance.username_or_email,
-  password: source_instance.password
-};
 
 async function connect() {
-    //TODO: add 2FA to console prompt
+    let twofa = prompt("Enter 2FA key (leave blank if 2FA not used): ")
+    let loginForm = {
+      username_or_email: source_instance.username_or_email,
+      password: source_instance.password,
+      totp_2fa_token: twofa
+    };
+    
     let login = await client.login(loginForm).catch((e) => { console.error(e); return false; });
     
-    console.log(login.jwt)
+    console.log(login.jwt);
 }
 
 connect()
