4d4a1cda64384ffaf92b89473bc3b6aed0f94e49
Robbie Irvine <rni2000@hw.ac.uk>
2023-07-04 15:02:56 +0100

    Improve login function and add more comments

--- index.js
+++ index.js
@@ -9,18 +9,37 @@ let srcUrl = srcInstance.url;
 let srcClient = new LemmyHttp(srcUrl);
 
 // logs into the given lemmy client using the given username and password, prompting for 2fa
-async function lemmyLogin(client, username, password) {
-  let twofa = prompt("Enter 2FA key (leave blank if 2FA not used): ")
+async function lemmyLogin(client, username, password, url) {
+  // 2FA prompt (adds url if passed)
+  let tfaPrompt = "Enter 2FA key"
+  if (url != undefined) tfaPrompt += " for " + (new URL(url)).hostname;
+  tfaPrompt += " (leave blank if 2FA not used): ";
+  
+  // constructs login form
+  let twofa = prompt(tfaPrompt)
   let loginForm = {
     username_or_email: username,
     password: password,
     totp_2fa_token: twofa
   };
-  return await client.login(loginForm).catch((e) => { console.error(e); return false; });
+
+  // returns login promise
+  return client.login(loginForm);
 }
 
+// Returns the name of the community in the format of community@instance, e.g. "asklemmy@lemmy.ml"
+function fetchCommunityName(cmnt) {
+  let cName = cmnt.name;
+  let cURL = new URL(cmnt.actor_id);
+  let cInst = cURL.hostname;
+
+  return cName + "@" + cInst;
+}
+
+// Main asynchronous function
 async function connect() {
-  let srcLogin = await lemmyLogin(srcClient, srcInstance.username_or_email, srcInstance.password);
+  let srcLogin = await lemmyLogin(srcClient, srcInstance.username_or_email, srcInstance.password, srcUrl)
+  .catch((e) => { console.error(e); return false; });
   
   //console.log(login.jwt);
   let siteData = await srcClient.getSite({auth: srcLogin.jwt}).catch((e) => { console.error(e); return false; });
@@ -31,15 +50,8 @@ async function connect() {
   for (const i of userData.follows) {
     console.log(fetchCommunityName(i.community));
   }
-}
 
-// Returns the name of the community in the format of community@instance, e.g. "asklemmy@lemmy.ml"
-function fetchCommunityName(cmnt) {
-  let cName = cmnt.name;
-  let cURL = new URL(cmnt.actor_id);
-  let cInst = cURL.hostname;
 
-  return cName + "@" + cInst;
 }
 
 connect();
