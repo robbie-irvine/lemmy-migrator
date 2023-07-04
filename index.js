9f45e5dfa2531d45726bd02e6a98f44d49bafdb7
Robbie Irvine <rni2000@hw.ac.uk>
2023-07-04 14:14:30 +0100

    Added function for getting community names

--- index.js
+++ index.js
@@ -9,19 +9,27 @@ let baseUrl = source_instance.url;
 let client = new LemmyHttp(baseUrl);
 
 async function connect() {
-    let twofa = prompt("Enter 2FA key (leave blank if 2FA not used): ")
-    let loginForm = {
-      username_or_email: source_instance.username_or_email,
-      password: source_instance.password,
-      totp_2fa_token: twofa
-    };
-    
-    let login = await client.login(loginForm).catch((e) => { console.error(e); return false; });
-    
-    //console.log(login.jwt);
-    let siteData = await client.getSite({auth: login.jwt}).catch((e) => { console.error(e); return false; });
-    let userData = siteData.my_user;
-    //console.log(siteData.my_user);
+  let twofa = prompt("Enter 2FA key (leave blank if 2FA not used): ")
+  let loginForm = {
+    username_or_email: source_instance.username_or_email,
+    password: source_instance.password,
+    totp_2fa_token: twofa
+  };
+  
+  let login = await client.login(loginForm).catch((e) => { console.error(e); return false; });
+  
+  //console.log(login.jwt);
+  let siteData = await client.getSite({auth: login.jwt}).catch((e) => { console.error(e); return false; });
+  let userData = siteData.my_user;
 }
 
-connect()
+// Returns the name of the community in the format of community@instance, e.g. "asklemmy@lemmy.ml"
+function fetchCommunityName(cmnt) {
+  let cName = cmnt.name;
+  let cURL = new URL(cmnt.actor_id);
+  let cInst = cURL.hostname;
+
+  return cName + "@" + cInst;
+}
+
+connect();
